10d9233b290332ecf4216a13f7891d9fac250e3c
HOUSSAM MNASFA <126343598+MNASFA@users.noreply.github.com>
2025-10-17 18:15:39 +0100

    Add files via upload

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hmnasfa <hmnasfa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 13:54:13 by hmnasfa           #+#    #+#             */
-/*   Updated: 2025/10/17 16:27:18 by hmnasfa          ###   ########.fr       */
+/*   Updated: 2025/10/17 18:12:53 by hmnasfa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void displayTable(PhoneBook &pb)
         std::string lastName = c.getLastName();
         std::string nickName = c.getNickName();
 
-        // truncate if longer than 10 carchters
         if (firstName.length() > 10)
             firstName = firstName.substr(0, 9) + ".";
         else if (lastName.length() > 10)
@@ -46,6 +45,29 @@ void displayTable(PhoneBook &pb)
     }
 }
 
+int is_printable(std::string str)
+{
+    for (int i = 0; str[i] ; i++)
+    {
+        if (str[i] > 32 && str[i] < 127)
+           return 1;
+    }
+    return 0;
+}
+
+int valid_number(std::string str)
+{
+    int i = 0;
+
+    while (str[i])
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return 0;
+        i++;
+    }
+    return 1;
+}
+
 void addContactPrompt(PhoneBook &pb)
 {
     std::string firstname;
@@ -54,41 +76,71 @@ void addContactPrompt(PhoneBook &pb)
     std::string phonenumber;
     std::string darkestsecret;
 
-    std::cout << "Enter First Name : ";
-    if (!std::getline(std::cin, firstname) || firstname.empty())
+    while (1)
     {
-        std::cout << "Error : First Name cannot be empty !" << std::endl;
-        return ;
-    }
-
-    std::cout << "Enter Last Name : ";
-    if (!std::getline(std::cin, lastname) || lastname.empty())
-    {
-        std::cout << "Error : Last Name cannot be empty !" << std::endl;
-        return ;
-    }
-
-    std::cout << "Enter Nickname : ";
-    if (!std::getline(std::cin, nickname) || nickname.empty())
-    {
-        std::cout << "Error : Nickname cannot be empty !" << std::endl;
-        return ;
-    }
-
-    std::cout << "Enter Phone Number : ";
-    if (!std::getline(std::cin, phonenumber) || phonenumber.empty())
-    {
-        std::cout << "Error : Phone Number cannot be empty !" << std::endl;
-        return ;
+        std::cout << "Enter First Name : ";
+        if (!std::getline(std::cin, firstname) || !is_printable(firstname))
+        {
+            std::cout << "Error : First Name cannot be empty !" << std::endl;
+            continue;
+        }
+        else
+            break;
     }
     
-    std::cout << "Enter Darkest Secret : ";
-    if (!std::getline(std::cin, darkestsecret) || darkestsecret.empty())
+    while (1)
     {
-        std::cout << "Error : Darkest Secret cannot be empty !" << std::endl;
-        return ;
+        std::cout << "Enter Last Name : ";
+        if (!std::getline(std::cin, lastname) || !is_printable(lastname))
+        {
+            std::cout << "Error : Last Name cannot be empty !" << std::endl;
+            continue;
+        }
+        else
+            break ;
     }
+    
 
+    while (1)
+    {
+        std::cout << "Enter Nickname : ";
+        if (!std::getline(std::cin, nickname) || !is_printable(nickname))
+        {
+            std::cout << "Error : Nickname cannot be empty !" << std::endl;
+            continue;
+        }
+        else
+            break ;
+    }
+    
+
+    while (1)
+    {
+        std::cout << "Enter Phone Number : ";
+        if (!std::getline(std::cin, phonenumber) || !is_printable(phonenumber) || !valid_number(phonenumber))
+        {
+            if (!valid_number(phonenumber))
+                std::cout << "Error : Phone Number must contain only digits !" << std::endl;
+            else
+                std::cout << "Error : Phone Number cannot be empty !" << std::endl;
+            continue;
+        }
+        else
+            break ;
+    }
+    
+    while (1)
+    {
+        std::cout << "Enter Darkest Secret : ";
+        if (!std::getline(std::cin, darkestsecret) || !is_printable(darkestsecret))
+        {
+            std::cout << "Error : Darkest Secret cannot be empty !" << std::endl;
+            continue ;
+        }
+        else
+            break ;
+    }
+    
     Contact newContact;
     newContact.setFirstName(firstname);
     newContact.setLastName(lastname);
@@ -114,6 +166,12 @@ void searchContact(PhoneBook &pb)
     std::cout << "Enter Contact Index : ";
     if (!std::getline(std::cin, indexStr))
         return ;
+        
+    if (!valid_number(indexStr))
+    {
+        std::cout << "Error : Invalid Index ! " << std::endl;
+        return ;
+    }
     
     int index = std::atoi(indexStr.c_str());
     
