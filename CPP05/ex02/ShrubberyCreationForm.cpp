26f53cd941ee4994307801e45ba6951086ff6a0d
Houssam Mnasfa <hmnasfa@e1r5p14.1337.ma>
2025-12-13 14:13:18 +0100

    Continue later

--- CPP05/ex02/ShrubberyCreationForm.cpp
+++ CPP05/ex02/ShrubberyCreationForm.cpp
@@ -5,3 +5,28 @@ ShrubberyCreationForm::ShrubberyCreationForm(const std::string &target)
         std::cout << "ShrubberyCreationForm constructor called for target" << std::endl;
 }
 
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &other)
+    : AForm(other), _target(other._target)
+{
+    std::cout << "ShrubberyCreationForm copy constructor called" << std::endl;
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &other){
+    std::cout << "ShrubberyCreationForm copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->operator=(other);
+    return *this;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(){
+    std::cout << "ShrubberyCreationForm destructor called for target " << _target  << std::endl;
+}
+
+std::string ShrubberyCreationForm::getTarget() const{
+    return this->_target;
+}
+
+void ShrubberyCreationForm::executeAction() const{
+    std::string filename = this->_target + "_shrubbery";
+    // {...}
+}
